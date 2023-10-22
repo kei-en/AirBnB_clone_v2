8df5c7152b216c45a695f6efc5bc6d42da97a51e
kei-en <njuguna0karanja@gmail.com>
2023-10-22 22:36:34 +0300

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """Defines the DBStorage engine."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 from os import getenv
 from models.base_model import Base
 from models.base_model import BaseModel
@@ -9,10 +13,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
